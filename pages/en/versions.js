eed140d1d4781acef3a7cdc82abaecf91eb91090
Pietro Dipalma <p.dipalma@cid.com>
2018-08-09 11:07:48 +0200

    fix siteconfig

--- pages/en/versions.js
+++ pages/en/versions.js
@@ -8,107 +8,86 @@
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary');
-
 const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
 
 const CWD = process.cwd();
 
-const siteConfig = require(`${CWD}/siteConfig.js`);
-const versions = require(`${CWD}/versions.json`);
+const siteConfig = require(CWD + '/siteConfig.js');
+const versions = require(CWD + '/versions.json');
 
-function Versions(props) {
-  const latestVersion = versions[0];
-  const repoUrl = `https://github.com/${siteConfig.organizationName}/${
-    siteConfig.projectName
-  }`;
-  return (
-    <div className="docMainWrapper wrapper">
-      <Container className="mainContainer versionsContainer">
-        <div className="post">
-          <header className="postHeader">
-            <h1>{siteConfig.title} Versions</h1>
-          </header>
-          <h3 id="latest">Current version (Stable)</h3>
-          <p>Latest version of Docusaurus.</p>
-          <table className="versions">
-            <tbody>
-              <tr>
-                <th>{latestVersion}</th>
-                <td>
-                  <a
-                    href={`${siteConfig.baseUrl}docs/${
-                      props.language
-                    }/installation`}>
-                    Documentation
-                  </a>
-                </td>
-                <td>
-                  <a href={`${repoUrl}/releases/tag/v${latestVersion}`}>
-                    Release Notes
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <h3 id="rc">Latest Version</h3>
-          Here you can find the latest documentation and unreleased code.
-          <table className="versions">
-            <tbody>
-              <tr>
-                <th>master</th>
-                <td>
-                  <a
-                    href={`${siteConfig.baseUrl}docs/${
-                      props.language
-                    }/next/installation`}>
-                    Documentation
-                  </a>
-                </td>
-                <td>
-                  <a href={repoUrl}>Source Code</a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <h3 id="archive">Past Versions</h3>
-          <p>
-            Here you can find documentation for previous versions of Docusaurus.
-          </p>
-          <table className="versions">
-            <tbody>
-              {versions.map(
-                version =>
-                  version !== latestVersion && (
-                    <tr key={version}>
-                      <th>{version}</th>
-                      <td>
-                        <a
-                          href={`${siteConfig.baseUrl}docs/${
-                            props.language
-                          }/${version}/installation`}>
-                          Documentation
-                        </a>
-                      </td>
-                      <td>
-                        <a href={`${repoUrl}/releases/tag/v${version}`}>
-                          Release Notes
-                        </a>
-                      </td>
-                    </tr>
-                  )
-              )}
-            </tbody>
-          </table>
-          <p>
-            You can find past versions of this project on{' '}
-            <a href={`${repoUrl}/releases`}>GitHub</a>.
-          </p>
-        </div>
-      </Container>
-    </div>
-  );
+class Versions extends React.Component {
+  render() {
+    const latestVersion = versions[0];
+    return (
+      <div className="docMainWrapper wrapper">
+        <Container className="mainContainer versionsContainer">
+          <div className="post">
+            <header className="postHeader">
+              <h2>{siteConfig.title + ' Versions'}</h2>
+            </header>
+            <p>New versions of this project are released every so often.</p>
+            <h3 id="latest">Current version (Stable)</h3>
+            <table className="versions">
+              <tbody>
+                <tr>
+                  <th>{latestVersion}</th>
+                  <td>
+                    <a href={''}>Documentation</a>
+                  </td>
+                  <td>
+                    <a href={''}>Release Notes</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              This is the version that is configured automatically when you
+              first install this project.
+            </p>
+            <h3 id="rc">Pre-release versions</h3>
+            <table className="versions">
+              <tbody>
+                <tr>
+                  <th>master</th>
+                  <td>
+                    <a href={''}>Documentation</a>
+                  </td>
+                  <td>
+                    <a href={''}>Release Notes</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p>Other text describing this section.</p>
+            <h3 id="archive">Past Versions</h3>
+            <table className="versions">
+              <tbody>
+                {versions.map(
+                  version =>
+                    version !== latestVersion && (
+                      <tr>
+                        <th>{version}</th>
+                        <td>
+                          <a href={''}>Documentation</a>
+                        </td>
+                        <td>
+                          <a href={''}>Release Notes</a>
+                        </td>
+                      </tr>
+                    )
+                )}
+              </tbody>
+            </table>
+            <p>
+              You can find past versions of this project{' '}
+              <a href="https://github.com/"> on GitHub </a>.
+            </p>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
-
-Versions.title = 'Versions';
 
 module.exports = Versions;
